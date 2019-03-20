541978f6aac0f6d421c26900af3caebed46201dc
Tam Nguyen <tamnguyenbbt@gmail.com>
2019-03-20 11:56:17 +1100

    Update SampleTests.cs

--- OkapiSampleTests/SampleTests.cs
+++ OkapiSampleTests/SampleTests.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Okapi.Drivers;
 using Okapi.Elements;
+using Okapi.Enums;
 
 namespace OkapiTests
 {
@@ -11,6 +13,51 @@ namespace OkapiTests
         public void Single_driver_auto_created_by_driver_pool()
         {
             DriverPool.Instance.ActiveDriver.LauchPage("https://www.xero.com/au/signup/");
+            var userName = TestObject.New("//label[span[contains(text(),'First name')]]/input");
+            userName.SendKeys("Automation");
+            DriverPool.Instance.QuitActiveDriver();
+        }
+
+        [TestMethod]
+        public void Developer_friendly_style()
+        {
+            DriverPool.Instance.ActiveDriver.LauchPage("https://www.xero.com/au/signup/");
+            var userName = new TestObject("//label[span[contains(text(),'First name')]]/input");
+            userName.SendKeys("Automation");
+            DriverPool.Instance.QuitActiveDriver();
+        }
+
+        [TestMethod]
+        public void XPath_by_default()
+        {
+            DriverPool.Instance.ActiveDriver.LauchPage("https://www.xero.com/au/signup/");
+            var userName = new TestObject("//label[span[contains(text(),'First name')]]/input");
+            userName.SendKeys("Automation");
+            DriverPool.Instance.QuitActiveDriver();
+        }
+
+        [TestMethod]
+        public void By_name()
+        {
+            DriverPool.Instance.ActiveDriver.LauchPage("https://www.xero.com/au/signup/");
+            var userName = new TestObject(LocatingMethod.Name, "FirstName"); //name attribute of tag input
+            userName.SendKeys("Automation");
+            DriverPool.Instance.QuitActiveDriver();
+        }
+
+        [TestMethod]
+        public void By_anchor()
+        {
+            DriverPool.Instance.ActiveDriver.LauchPage("https://www.xero.com/au/signup/");
+            var userName = TestObject.New(SearchInfo.New("span", "First name"), SearchInfo.New("input"));
+            userName.SendKeys("Automation");
+            DriverPool.Instance.QuitAllDrivers();
+        }
+
+        [TestMethod]
+        public void One_dynamic_content_making_one_test_object_to_hit_two_fields()
+        {
+            DriverPool.Instance.ActiveDriver.LauchPage("https://www.xero.com/au/signup/");
             var userName = TestObject.New(SearchInfo.New("span", "{0}"), SearchInfo.New("input"), DynamicContents.New("First name"));
             userName.SendKeys("Automation");
             userName.DynamicContents = DynamicContents.New("Last name");
@@ -19,10 +66,61 @@ namespace OkapiTests
         }
 
         [TestMethod]
-        public void Single_driver_auto_created_by_driver_pool_plus_user_created_driver()
+        public void Another_develper_friendly_style_by_anchor_implicitly()
+        {
+            ManagedDriver currentDriver = DriverPool.Instance.ActiveDriver;
+            currentDriver.LauchPage("https://www.xero.com/au/signup/");
+
+            var userName = new TestObject()
+            {
+                AnchorElementInfo = SearchInfo.New("span", "{0}"),
+                SearchElementInfo = SearchInfo.New("input"),
+                DynamicContents = new List<string>() { "First name" }
+            };
+
+            userName.SendKeys("Automation");
+            userName.DynamicContents = DynamicContents.New("Last name");
+            userName.SendKeys("Tester");
+            DriverPool.Instance.Quit(currentDriver);
+        }
+
+        [TestMethod]
+        public void Another_develper_friendly_style_by_xpath_as_default()
+        {
+            ManagedDriver currentDriver = DriverPool.Instance.ActiveDriver;
+            currentDriver.LauchPage("https://www.xero.com/au/signup/");
+
+            var userName = new TestObject()
+            {
+                Locator = "//label[span[contains(text(),'{0}')]]/input",
+                DynamicContents = new List<string>() { "First name" }
+            };
+
+            userName.SendKeys("Automation");
+            userName.DynamicContents = DynamicContents.New("Last name");
+            userName.SendKeys("Tester");
+            DriverPool.Instance.Quit(currentDriver);
+        }
+
+        [TestMethod]
+        public void Another_develper_friendly_style_by_name()
         {
             DriverPool.Instance.ActiveDriver.LauchPage("https://www.xero.com/au/signup/");
 
+            var userName = new TestObject()
+            {
+                LocatingMethod = LocatingMethod.Name,
+                Locator = "FirstName"
+            };
+
+            userName.SendKeys("Automation");
+            DriverPool.Instance.QuitActiveDriver();
+        }
+
+        [TestMethod]
+        public void Single_driver_auto_created_by_driver_pool_plus_user_created_driver()
+        {
+            DriverPool.Instance.ActiveDriver.LauchPage("https://www.xero.com/au/signup/");
             var userName = TestObject.New("//label[span[contains(text(),'{0}')]]/input", DynamicContents.New("First name"));
             ManagedDriver previousActiveDriver = DriverPool.Instance.ActiveDriver;
             DriverPool.Instance.CreateDriver().LauchPage("https://www.google.com");
