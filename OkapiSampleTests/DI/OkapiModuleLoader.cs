753a2b24b28a10011812494ba32e02b97e332232
tamnguyenbbt@gmail.com <tamnguyenbbt@gmail.com>
2019-03-21 02:35:49 +1100

    Add DI

---
+++ OkapiSampleTests/DI/OkapiModuleLoader.cs
@@ -0,0 +1,22 @@
+using Ninject;
+using Okapi.Configs;
+using Okapi.DI;
+using Okapi.Drivers;
+using Okapi.Logs;
+using OkapiSampleTests.Configurations;
+using OkapiSampleTests.ThirdParties;
+using OkapiTests;
+
+namespace OkapiSampleTests.DI
+{
+    internal class OkapiModuleLoader : IOkapiModuleLoader
+    {
+        public void LoadAssemblyBindings(IKernel kernel)
+        {
+            kernel.Bind<ITestEnvironment>().To<LocalChromeTestEnvironment>().InSingletonScope();
+            kernel.Bind<IDriverConfig>().To<DriverConfig>().InSingletonScope();
+            kernel.Bind<IDriverOptionsFactory>().To<DriverOptionsFactory>().InSingletonScope();
+            kernel.Bind<ILoggerConfigurationProvider>().To<LoggerConfigurationProvider>().InSingletonScope();
+        }
+    }
+}
