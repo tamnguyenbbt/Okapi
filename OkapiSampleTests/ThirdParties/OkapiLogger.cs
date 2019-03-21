d5bddaf8dbd00a3c206436311c98c6f41a194e80
tamnguyenbbt@gmail.com <tamnguyenbbt@gmail.com>
2019-03-21 22:27:30 +1100

    Update

---
+++ OkapiSampleTests/ThirdParties/OkapiLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Okapi.Logs;
+using Serilog;
+using Serilog.Core;
+
+namespace OkapiSampleTests.ThirdParties
+{
+    internal class OkapiLogger : IOkapiLogger
+    {
+        private readonly static Logger logger = new LoggerConfiguration().WriteTo.File("C:\\DEV\\Tam\\Okapi\\log.txt").CreateLogger();
+
+        public void Error(string messageTemplate)
+        {
+            logger.Error(messageTemplate);
+        }
+
+        public void Error(string messageTemplate, Exception exception)
+        {
+            logger.Error(exception, messageTemplate);
+        }
+
+        public void Info(string messageTemplate)
+        {
+            logger.Information(messageTemplate);
+        }
+    }
+}
