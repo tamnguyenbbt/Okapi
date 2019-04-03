b45c58957d09fbe40e172ff64f2e1bd2ac89547e
Tam Nguyen <tamnguyenbbt@gmail.com>
2019-04-03 16:53:52 +1100

    Update DriverConfig.cs

--- OkapiSampleTests/ProjectConfig/DriverConfig.cs
+++ OkapiSampleTests/ProjectConfig/DriverConfig.cs
@@ -6,8 +6,6 @@ namespace OkapiSampleTests.ProjectConfig
     internal class DriverConfig : IDriverConfig
     {
         public int TimeoutInSeconds => 2;
-        public bool QuitDriverOnError => true;
-        public bool QuitDriverOnFailVerification => true;
         public DomUtilConfig SearchByAnchorConfig => null;        
     }
 }
